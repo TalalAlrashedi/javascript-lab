75f2ea1634af5cbea2f99bdcdcd8e39588b08df1
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-04 10:50:11 +0300

    q2 done

---
+++ main.js
@@ -0,0 +1,18 @@
+let heightOfsquare = 6;
+let widthOfsquare = 4;
+
+let total = heightOfsquare * widthOfsquare;
+
+console.log(`The area of the rectangle is: ${total} `);
+
+console.log("=======================================================");
+
+let celDegree = 25;
+
+let fahernhitDegree = (celDegree * 9) / 5 + 32;
+console.log(`${celDegree}°C is NN°F is ${fahernhitDegree}`);
+
+let fahernhitDegree2 = 70;
+let celCompareToFaheerhint = ((fahernhitDegree2 - 32) * 5) / 9;
+
+console.log(`${fahernhitDegree2}°C is NN°F is ${celDegree}`);
