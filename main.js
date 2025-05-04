113e6b0f52da02bb73b746d996db5c797507cea5
TalalAlrashedi <TalalAlrashedi24@hotmail.com>
2025-05-04 11:03:28 +0300

    6 done

--- main.js
+++ main.js
@@ -36,6 +36,17 @@ console.log(thingTwo);
 console.log("=======================================================");
 
 let bill = 10.25 + 3.99 + 7.15;
-let tip = 15%
-console.log(tip);
+let tip = 15
 let totalOfcost = bill + tip
+
+console.log(totalOfcost);
+
+
+console.log("=======================================================");
+let adjective1 = "amazing";
+let adjective2 = "fun";
+let adjective3 = "entertaining";
+
+let madLib = `The Intro to JavaScript course is ${adjective1}. James and Julia are so ${adjective2} I cannot wait to work through the rest of this ${adjective3} content!`
+
+console.log(madLib);
